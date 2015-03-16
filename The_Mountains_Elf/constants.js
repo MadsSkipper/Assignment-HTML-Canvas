5f64859142c2cb6fa3b3755f8a9d54d51ef3d49a
MadsSkipper <bretonnia@ofir.dk>
2015-03-16 13:29:11 +0100

    spil

---
+++ The_Mountains_Elf/constants.js
@@ -0,0 +1,43 @@
+﻿//------------
+//System Values
+//------------
+var STAGE_WIDTH = 640,
+	STAGE_HEIGHT = 480,
+	TIME_PER_FRAME = 60, //this equates to 30 fps
+	GAME_FONTS = "bold 20px sans-serif";
+
+var PATH_CHAR = "img/movesim.png";
+
+var CHAR_WIDTH = 100,
+	CHAR_HEIGHT = 150,
+    CHAR_X = 200,
+CHAR_Y = 200,
+	CHAR_START_X = 200,
+	CHAR_START_Y = 200,
+	CHAR_SPEED = 5,
+	IMAGE_START_X = 0,
+	IMAGE_START_NORTH_Y = 0,
+	IMAGE_START_EAST_Y = 150,
+	IMAGE_START_SOUTH_Y = 300,
+	IMAGE_START_WEST_Y = 450,
+	SPRITE_WIDTH = 216;
+
+var TEXT_PRELOADING = "Loading ...",
+	TEXT_PRELOADING_X = 200,
+	TEXT_PRELOADING_Y = 200;
+var bor = "img/bor.png";
+
+var bor_width = 50,
+    bor_height = 50.
+botCHAR_X = 150,
+borCHAR_Y = 150,
+borCHAR_START_X = 150,
+	borCHAR_START_Y = 150,
+borIMAGE_START_X = 0,
+	borIMAGE_START_NORTH_Y = 0,
+	borIMAGE_START_EAST_Y = 0,
+	borIMAGE_START_SOUTH_Y = 0,
+	borIMAGE_START_WEST_Y = 0,
+	borSPRITE_WIDTH = 216;
+
+
