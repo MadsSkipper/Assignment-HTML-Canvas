5ef91ba5920619ac404156d3901c108607af2049
MadsSkipper <bretonnia@ofir.dk>
2015-03-18 08:16:27 +0100

    vs1

--- The_Mountains_Elf/game.js
+++ The_Mountains_Elf/game.js
@@ -6,14 +6,19 @@ stage.width = STAGE_WIDTH;
 stage.height = STAGE_HEIGHT;
 var ctx = stage.getContext("2d");
 ctx.fillStyle = "grey";
+bagImage = new Image();
+bagImage.src = "img/gru.png";
 ctx.font = GAME_FONTS;
 var arrBitGameFieldy = [];
 var arrBitGameFieldx = [];
 for (var i = 0; i < 481; i++) {
-    arrBitGameFieldy[i] = [];
-    
-}
+    arrBitGameFieldy[i] = 0;
 
+}
+for (var i = 0; i < 641; i++) {
+    arrBitGameFieldx[i] = 0;
+
+}
 var bitEmpty = 0x0;	//00000000 0000000
 var bitFull = 0xFFFF;	//11111111 1111111
 //---------------
@@ -21,10 +26,17 @@ var bitFull = 0xFFFF;	//11111111 1111111
 //---------------
 //Preload Art Assets
 // - Sprite Sheet
-var charImage = new Image();
+var charImage,
+charImage = new Image();
 charImage.ready = false;
 charImage.onload = setAssetReady;
-charImage.src = PATH_CHAR;
+charImage.src = PATH_CHAR
+//charImage = {
+
+//    x: 200,
+//    y: 200
+//};
+
 
 var borImage = new Image();
 borImage.ready = false;
@@ -39,7 +51,6 @@ var palsy;
 function setAssetReady() {
     this.ready = true;
 }
-
 //Display Preloading
 ctx.fillRect(0, 0, stage.width, stage.height);
 ctx.fillStyle = "#000";
@@ -104,7 +115,7 @@ CHAR_Y = charY;
 CHAR_X = charX;
 borcharX = borCHAR_START_X;
 borcharY = borCHAR_START_Y;
-
+POS_SPEED = 10;
 currX = IMAGE_START_X;
 currY = IMAGE_START_EAST_Y;
 
@@ -117,27 +128,32 @@ function update() {
     ctx.fillRect(0, 0, stage.width, stage.height);
 
     if (isMoving) {
-        if (facing == "N") {           
+        if (facing == "N") {
             charY -= CHAR_SPEED;
-            CHAR_Y -= CHAR_SPEED;
+            CHAR_Y -= POS_SPEED;
             currY = IMAGE_START_NORTH_Y;
+            charImage.y -= CHAR_SPEED;
+
         }
-        else if (facing == "E") {            
+        else if (facing == "E") {
             charX += CHAR_SPEED;
-            CHAR_X += CHAR_SPEED;
+            CHAR_X += POS_SPEED;
             currY = IMAGE_START_EAST_Y;
+            charImage.x += CHAR_SPEED;
         }
-        else if (facing == "S") {           
-                           
-                charY += CHAR_SPEED;
-                CHAR_Y += CHAR_SPEED;
-                currY = IMAGE_START_SOUTH_Y;
-            
+        else if (facing == "S") {
+
+            charY += CHAR_SPEED;
+            CHAR_Y += POS_SPEED;
+            currY = IMAGE_START_SOUTH_Y;
+            charImage.y += CHAR_SPEED;
+
         }
-        else if (facing == "W") {            
+        else if (facing == "W") {
             charX -= CHAR_SPEED;
-            CHAR_X -= CHAR_SPEED;
-            currY = IMAGE_START_WEST_Y;            
+            CHAR_X -= POS_SPEED;
+            currY = IMAGE_START_WEST_Y;
+            charImage.x -= CHAR_SPEED;
         }
 
         currX += CHAR_WIDTH;
@@ -146,70 +162,98 @@ function update() {
             currX = 0;
     }
     // Are they touching?
-    y = CHAR_Y;
-    X = CHAR_X;
-    if (arrBitGameFieldy[y] != bitEmpty)
-    {
-        if (facing == "N") {
-            charY += CHAR_SPEED;
-            CHAR_Y += CHAR_SPEED;
-            currY = IMAGE_START_SOUTH_Y;
 
-        }
-        else if (facing == "E") {
-            charX -= CHAR_SPEED;
-            CHAR_X -= CHAR_SPEED;
-            currY = IMAGE_START_WEST_Y;
-        }
-        else if (facing == "S") {
 
-            charY -= CHAR_SPEED;
-            CHAR_Y -= CHAR_SPEED;
-            currY = IMAGE_START_NORTH_Y;
+    if (arrBitGameFieldy[charY] != bitEmpty && arrBitGameFieldx[charX] != bitEmpty) {
+        
+            if (facing == "N") {
+                charY = 150;
+                charY += CHAR_SPEED;
+                CHAR_Y += POS_SPEED;
+                currY = IMAGE_START_SOUTH_Y;
 
-        }
-        else if (facing == "W") {
-            charX += CHAR_SPEED;
-            CHAR_X += CHAR_SPEED;
-            currY = IMAGE_START_EAST_Y;
+            }
+            else if (facing == "E") {
+                charX = 150;
+                charX -= CHAR_SPEED;
+                CHAR_X -= POS_SPEED;
+                currY = IMAGE_START_WEST_Y;
+            }
+            else if (facing == "S") {
+                charY = 150;
+                charY -= CHAR_SPEED;
+                CHAR_Y -= POS_SPEED;
+                currY = IMAGE_START_NORTH_Y;
 
-        }
+            }
+            else if (facing == "W") {
+                charX = 150;
+                charX += CHAR_SPEED;
+                CHAR_X += POS_SPEED;
+                currY = IMAGE_START_EAST_Y;
+
+            }
+        
+       
+
     }
     else {
-        
-    }
-    
-   
-    
-        
 
-    
+
+    }
+
+
+
+
+
+
 
 
     //Draw Image
-    var pals = 0;    
-        for (var i = 0; i < 5; i++) {
-            ctx.drawImage(borImage, borcurrX, borcurrY, bor_width, bor_height, pals, borcharY, bor_width, bor_height);
-            pals = pals + 50;
-            y = borcharY;
-            x = pals
-            arrBitGameFieldy[y] = arrBitGameFieldy[y] | (1 << x);            
-        }         
-        pals = pals + 150;
-        for (var i = 0; i < 5; i++) {
-            ctx.drawImage(bor2Image, borcurrX, borcurrY, bor_width, bor_height, pals, borcharY, bor_width, bor_height);
-            pals = pals + 50;
-            y = borcharY;
-            x = pals
+    ctx.drawImage(bagImage, 0, 0, 640, 480, 0, 0, 640, 480);
+    var pals = 0, palsII = 400;
+    for (var i = 0; i < 5; i++) {
+        ctx.drawImage(borImage, borcurrX, borcurrY, bor_width, bor_height, pals, borcharY, bor_width, bor_height);
+        y = borcharY;
+        x = pals;
+        pals = pals + 50;       
             arrBitGameFieldy[y] = arrBitGameFieldy[y] | (1 << x);
+        
+        for(var j = x; j <= pals; j++)
+        {
+            arrBitGameFieldx[j] = arrBitGameFieldx[x] | (1 << y);
+        
         }
-     
-    palsy = borcharY;
-     
+
+    }
+    
+    for (var i = 0; i < 5; i++) {
+        ctx.drawImage(bor2Image, borcurrX, borcurrY, bor_width, bor_height, palsII, borcharY, bor_width, bor_height);
+        y = borcharY;
+        x = palsII;
+        palsII = palsII + 50;        
+            arrBitGameFieldy[y] = arrBitGameFieldy[y] | (1 << x);        
+        if (x == 400) {
+            for (var j = 350; j <= palsII; j++) {
+                arrBitGameFieldx[j] = arrBitGameFieldx[x] | (1 << y);
+
+            }
+        }
+        else {
+            for (var j = x; j <= palsII; j++) {
+                arrBitGameFieldx[j] = arrBitGameFieldx[x] | (1 << y);
+
+            }
+        }
+
+    }
+
+
     ctx.drawImage(charImage, currX, currY, CHAR_WIDTH, CHAR_HEIGHT,
 					charX, charY, CHAR_WIDTH, CHAR_HEIGHT);
 
-   
+
+
 }
 
 
